50f2219baa7700d74d99428109a64c8720e3fb64
D-K-E <D-K-E@users.noreply.github.com>
2022-01-07 22:52:57 +0100

    shader module refactoring continues
    Shader module refactoring continues. Pipelineshaderstage struct
    shall be written. The flags can be passed as a template argument
    but also constexpr arrays might be an option as well

--- include/vkshader/vkshadermodule.hpp
+++ include/vkshader/vkshadermodule.hpp
@@ -28,6 +28,17 @@ addressed by pCode.
 struct ShaderModuleCreateInfoVk {
   VkShaderModuleCreateInfo createInfo;
   //
+
+  template <typename T> void set(std::optional<T> &pNext) {
+    if (pNext.has_value()) {
+      auto val = pNext.value();
+      if (std::is_pointer(T)) {
+        createInfo.pNext = static_cast<void *>(val);
+      } else {
+        createInfo.pNext = static_cast<void *>(&val);
+      }
+    }
+  }
   ShaderModuleCreateInfoVk(const std::array<char> &shaderCode) {
     //
     createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
@@ -41,7 +52,38 @@ struct ShaderModuleCreateInfoVk {
     createInfo.pCode = reinterpret_cast<const uint32_t *>(shaderCode.data());
   }
 };
+/**
+typedef struct VkPipelineShaderStageCreateInfo {
+ 
+VkStructureType sType;
+ 
+const void* pNext;
+ 
+VkPipelineShaderStageCreateFlags flags;
+ 
+VkShaderStageFlagBits stage;
+ 
+VkShaderModule module;
+ 
+const char* pName;
+ 
+const VkSpecializationInfo* pSpecializationInfo;
+} VkPipelineShaderStageCreateInfo;
 
-
+- sType is the type of this structure.
+- pNext is NULL or a pointer to a structure extending this structure.
+- flags is a bitmask of VkPipelineShaderStageCreateFlagBits specifying how the
+pipeline shader stage will be generated.
+- stage is a VkShaderStageFlagBits value specifying a single pipeline stage.
+- module is a VkShaderModule object containing the shader for this stage.
+- pName is a pointer to a null-terminated UTF-8 string specifying the entry
+point name of the shader for this stage.
+- pSpecializationInfo is a pointer to a VkSpecializationInfo structure, as
+described in Specialization Constants, or NULL.
+ */
+struct PipelineShaderStageCreateInfoVk {
+  //
+  VkPipelineShaderStageCreateInfo createInfo;
+};
 
 }; // namespace vtuto
